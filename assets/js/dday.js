2558c3bf8cda3428209ec40259830a268710cd0b
soon-haari <alstns.040422@gmail.com>
2023-09-01 01:32:22 +0900

    gift

--- assets/js/dday.js
+++ assets/js/dday.js
@@ -2,18 +2,22 @@ const p = document.createElement("p");
 p.style.textAlign = "center";
 p.style.fontSize = "13pt";
 
-var gift = new Date(2023, 8, 26);
-var now = new Date();
+var gift_year = 2023;
+var gift_month = 8;
+var gift_day = 26;
 
+var now = new Date();
 var year = now.getFullYear();
 var month = now.getMonth() + 1;
 var day = now.getDate();
 
-var today = new Date(year, month, day);
+var gift = new Date(gift_year, gift_month - 1, gift_day);
+var today = new Date(year, month - 1, day);
 
 diff_time = today.getTime() - gift.getTime()
 diff_date = diff_time / (1000 * 60 * 60 * 24)
 
 p.innerHTML = `08/26<br><br>💚💚<br><br>D + ${diff_date}`;
+//p.innerHTML = `${today} ${gift}`
 document.body.append(p);
 
